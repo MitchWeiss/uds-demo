b3468a4439003f4912c481cd6f463b55e4271a47
Mitchell Weiss <mitchell.weiss@airtasker.com.au>
2018-11-27 13:41:15 +1100

    Remove redux logic

--- src/epics/index.js
+++
@@ -1,4 +0,0 @@
-import { combineEpics } from "redux-observable";
-import { fetchTasks, fetchTask, claimTask } from "./tasks";
-
-export default combineEpics(fetchTasks, fetchTask, claimTask);
--- src/sagas/index.js
+++
@@ -1,6 +0,0 @@
-import { all } from "redux-saga/effects";
-import { watchFetchTasks, watchFetchTask, watchClaimTask } from "./tasks";
-
-export default function* rootSaga() {
-  yield all([watchFetchTasks(), watchFetchTask(), watchClaimTask()]);
-}
